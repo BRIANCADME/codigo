2ea7224a671881026ad79d9e968f3d813974fe3b
BRIANCADME <tenempaguaybrian02@gmail.com>
2021-07-22 18:34:37 -0500

    example

---
+++ echo por mi poniendo mayo en vez de 05.py
@@ -0,0 +1,11 @@
+with open ("Concrete_Data(1).csv") as f:
+    print(f.read())
+ #   primera_linea = True
+   # for lines in f:
+      #  if not primera_linea:
+            
+            
+       #     cemento = lines.split(";")[0]
+        
+       #resistencia = lines.split(";")[-1]
+            
